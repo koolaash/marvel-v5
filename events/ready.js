6de6c41d9333896d97d92ac244e3674628fec036
koolaash <73039811+koolaash@users.noreply.github.com>
2022-04-27 23:13:09 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 module.exports.run = async (client) => {
     console.log(`[APPLICATION] => ${client.user.tag} IS READY TO BE USED`.yellow)
     client.noprefix = await client.qdb.get(`noprefix.mem`)
+    client.partner = await client.qdb.get(`partner.mem`)
 }
