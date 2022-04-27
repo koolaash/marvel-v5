fed8958d63f08e4e17b5e669405de4f707e9613b
koolaash <73039811+koolaash@users.noreply.github.com>
2022-04-27 15:38:00 +0530

    Update serverinfo.js

--- commands/INFORMATION/serverinfo.js
+++ commands/INFORMATION/serverinfo.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports = {
     name: "serverinfo",
-    aliases: ["server"],
+    aliases: ["server", "si"],
     description: "All the details about server",
     category: "INFORMATION",
     usage: "serverinfo",
