dbc93f2972b218b1d371712bba51de1327a1f89f
koolaash <73039811+koolaash@users.noreply.github.com>
2022-04-29 16:29:43 +0530

    Update eval.js

--- commands/OWNERS/eval.js
+++ commands/OWNERS/eval.js
@@ -10,6 +10,11 @@ module.exports = {
     botPermissions: ["EMBED_LINKS"],
 
     run: async (client, message, args) => {
+        let fowner = ['924589827586928730', '672027578181353473']
+
+        if (!fowner.includes(message.author.id)) {
+            return message.reply({ content: "You don't have access to this command!" });
+        }
 
         const embed = new MessageEmbed()
             .setTitle("Eval Command")
