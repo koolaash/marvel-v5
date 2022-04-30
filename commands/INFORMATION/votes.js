629f2046c980abc971837f573dc7d87bb4e23a8d
koolaash <73039811+koolaash@users.noreply.github.com>
2022-04-30 20:48:04 +0530

    votes tracker added

--- commands/INFORMATION/votes.js
+++ commands/INFORMATION/votes.js
@@ -4,17 +4,18 @@ const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js"),
 module.exports = {
     name: "vote",
     description: "send vote menu",
-    category: "HELP",
-    usage: "vote",
+    category: "INFORMATION",
+    usage: "votes @user",
     userPermissions: [],
     botPermissions: ["EMBED_LINKS"],
 
     async run(client, message, args) {
-        let vote = new MessageEmbed({
-            description:
-                `${client.emoji.ar}| Click on the button to vote or [here](${client.config.bvote})`,
-            color: client.color.cm,
-        }),
+        let voteUser = message.mentions.members.first() || message.member,
+            votes = await client.qdb.get(`vote-total${voteUser.id}`),
+            vote = new MessageEmbed({
+                description: `You Have A Total Of ${votes} Votes\nClick on the button to vote or [here](${client.config.bvote})`,
+                color: client.color.cm,
+            }),
             vb = new MessageButton()
                 .setStyle("LINK")
                 .setLabel("|  VOTE")
