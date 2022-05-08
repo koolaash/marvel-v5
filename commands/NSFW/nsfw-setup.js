68838e237b6afa107ab384b9afb580694d60b15f
koolaash <73039811+koolaash@users.noreply.github.com>
2022-05-09 02:09:30 +0530

    Update nsfw-setup.js

--- commands/NSFW/nsfw-setup.js
+++ commands/NSFW/nsfw-setup.js
@@ -75,36 +75,25 @@ module.exports = {
         }
         let options = [],
             option0 = { label: 'Hentai Ass', value: '0' },
-            option1 = { label: 'Hentai Anal', value: '1' },
-            option2 = { label: 'Hentai Pussy', value: '2' },
-            option3 = { label: 'Hentai Midriff', value: '3' },
-            option4 = { label: 'Hentai Boobs', value: '4' },
-            option5 = { label: 'Hentai Thigs', value: '5' },
-            option6 = { label: 'Hentai Random', value: '6' },
-            option7 = { label: 'Neko Nero', value: '7' },
-            option8 = { label: 'Neko Feet', value: '8' },
-            option9 = { label: 'Neko Lewd', value: '9' },
-            option10 = { label: 'Neko Pussy', value: '10' },
-            option11 = { label: 'Neko Solo', value: '11' },
-            option12 = { label: 'Neko Blowjob', value: '12' },
-            option13 = { label: 'Neko Cumart', value: '13' },
-            option14 = { label: 'Neko Cumslut', value: '14' },
-            option15 = { label: 'Neko Futanari', value: '15' },
-            option16 = { label: 'Neko Lesbian', value: '16' },
-            option17 = { label: 'Neko Boobs', value: '17' },
-            option18 = { label: 'Porn Anal', value: '18' },
-            option19 = { label: 'Porn Ass', value: '19' },
-            option20 = { label: 'Porn Boobs', value: '20' },
-            option21 = { label: 'Porn 4k', value: '21' },
-            option22 = { label: 'Porn Pussy', value: '22' },
-            option23 = { label: 'Porn Gif', value: '23' },
-            option24 = { label: 'Porn Thigh', value: '24' };
+            option1 = { label: 'Hentai Midriff', value: '1' },
+            option2 = { label: 'Hentai Thigs', value: '2' },
+            option3 = { label: 'Hentai Random', value: '3' },
+            option4 = { label: 'Porn Anal', value: '4' },
+            option5 = { label: 'Porn Ass', value: '5' },
+            option6 = { label: 'Porn Boobs', value: '6' },
+            option7 = { label: 'Porn 4k', value: '7' },
+            option8 = { label: 'Porn Pussy', value: '8' },
+            option9 = { label: 'Porn Gif', value: '9' },
+            option10 = { label: 'Porn Thigh', value: '10' },
+            option11 = { label: 'Random Image', value: '11' },
+            option12 = { label: 'Random Gif', value: '12' },
+            option13 = { label: 'Random Video', value: '13' },
+            option14 = { label: 'GoneWild', value: '14' };
 
         options.push(
             option0, option1, option2, option3, option4, option5, option6,
-            option7, option8, option9, option10, option11, option12,
-            option13, option14, option15, option16, option17, option18,
-            option19, option20, option21, option22, option23, option24
+            option7, option8, option9, option10, option11, option12, option13,
+            option14
         )
         let menu = new MessageSelectMenu()
             .setPlaceholder('Select Nsfw Type')
