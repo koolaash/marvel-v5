cef53660a4f70f90d26159a5a0493078ccc5d825
Damon <73039811+koolaash@users.noreply.github.com>
2022-07-22 16:39:30 +0530

    Create ping.js

---
+++ SlashCommands/INFO/ping.js
@@ -0,0 +1,15 @@
+const { MessageEmbed, Client, CommandInteraction } = require('discord.js');
+
+module.exports = {
+    name: 'ping',
+    description: 'shows the bot ping',
+    userPermissions: [''],
+    /** 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     * @param {String[]} args 
+     */
+    run: async (client, interaction, args) => {
+        interaction.reply({ content: `${client.ws.ping}ms`, ephemeral: true })
+    },
+};
