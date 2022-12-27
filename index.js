0766c9c60296b6c577b792929a075e7aacf3f100
Damon <73039811+koolaash@users.noreply.github.com>
2022-12-28 00:29:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const Discord = require("discord.js"),
-  { Intents, WebhookClient, MessageEmbed, MessageActionRow, MessageButton, Collection, Client } = require("discord.js"),
+const { readdirSync } = require("fs"),
+  { Intents, WebhookClient, Collection, Client } = require("discord.js"),
+  config = require('./json/config.json'),
   intents = new Intents([
-    "GUILD_MEMBERS",
-    "GUILD_MESSAGES",
-    "DIRECT_MESSAGES",
-    "GUILDS",
-    "GUILD_MESSAGE_REACTIONS",
-    "DIRECT_MESSAGE_REACTIONS"
+    "GUILD_MEMBERS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILDS",
+    "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGE_REACTIONS",
+    "GUILD_BANS", "MESSAGE_CONTENT", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS"
   ]),
   client = new Client({
     intents: [
@@ -15,9 +13,16 @@ const Discord = require("discord.js"),
       Intents.FLAGS.GUILD_MESSAGES,
       Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
       Intents.FLAGS.GUILD_MEMBERS,
-      Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
+      Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+      Intents.FLAGS.GUILD_BANS,
+      Intents.FLAGS.MESSAGE_CONTENT,
+      Intents.FLAGS.GUILD_INTEGRATIONS,
+      Intents.FLAGS.GUILD_WEBHOOKS
     ],
-    allowedMentions: { parse: ['users'], repliedUser: true },
+    allowedMentions: {
+      parse: ['users'],
+      repliedUser: false
+    },
     presence: {
       status: "idle",
       activities: [{
@@ -32,20 +37,18 @@ const Discord = require("discord.js"),
     restWsBridgetimeout: 100,
     disableEveryone: true,
     partials: [
-      'MESSAGE',
-      'CHANNEL',
-      'REACTION',
-      'GUILD_MEMBER',
-      'USER'
+      'MESSAGE', 'CHANNEL',
+      'REACTION', 'GUILD_MEMBER',
+      'USER', "GUILD_SCHEDULED_EVENT"
     ]
   }),
   del = new WebhookClient({
-    id: "963519810015215670",
-    token: "vLEb5pYo6jGT3qoPGtdbe5QAk_QNG85XilbeUSE7r1YijKISTQ4mDiAoHdaegcBRhnOS"
+    id: config.del.id || process.env.del_id,
+    token: config.del.token || process.env.del_token
   }),
   welweb = new WebhookClient({
-    id: '948234227000942622',
-    token: 'Yr4qzFRYpS_Sh8KPQW6RcNWrQ7MlZYQkI6_bkhtuVz-L1w3ktYrOceRKAgJwt4Lfun_I'
+    id: config.wel.id || process.env.wel_id,
+    token: config.wel.token || process.env.wel_token
   });
 
 client.commands = new Collection();
@@ -62,13 +65,14 @@ client.role = require("./json/roles.json");
 client.error = require("./json/errors.json");
 client.del = del;
 client.welweb = welweb;
+
 require("colors");
 
 const mongoose = require("mongoose"),
   dbOptions = {
     useNewUrlParser: true,
     autoIndex: false,
-    poolSize: 5,
+    poolSize: 8,
     connectTimeoutMS: 10000,
     family: 4,
     useUnifiedTopology: true,
@@ -94,11 +98,23 @@ const { Database } = require("quickmongo"),
   });
 
 client.qdb = new Database(client.config.DB || process.env.DB);
-client.qdb.on("ready", async () => {
-  console.log(`MONGOOSE QUICK DAATABASE CONNECTED`.yellow);
-});
+client.qdb.on("ready", async () => { console.log(`MONGOOSE QUICK DAATABASE CONNECTED`.yellow); });
 client.errweb = errweb;
 client.prefixModel = require('./models/prefixes.js');
+
+// webhooks for errors
+client.dataError = new WebhookClient({
+  id: process.env.dataError_id || config.dataError.id,
+  token: process.env.dataError_token || config.dataError.token,
+});
+client.auditError = new WebhookClient({
+  id: process.env.auditError_id || config.auditError.id,
+  token: process.env.auditError_token || config.auditError.token,
+});
+client.antiNukeError = new WebhookClient({
+  id: process.env.antinukeError_id || config.antinukeError.id,
+  token: process.env.antinukeError_token || config.antinukeError.token,
+});
 
 require('events').EventEmitter.defaultMaxListeners = 100;
 process.setMaxListeners(100);
@@ -106,13 +122,22 @@ process.setMaxListeners(100);
   require(`./handlers/${handler}`)(client);
 });
 
-require('./alive.js');
-require('./interactions/button.js')(client);
-require('./interactions/drop-down.js')(client);
-require("./events2/snipe.js")(client);
-require("./events2/autorole.js")(client);
-require("./events2/embedwel.js")(client);
-require("./events2/moderation.js")(client);
+// require('./alive.js');
+
+readdirSync("./interactions/").forEach(file => {
+  require(`./interactions/${file}`)(client);
+});
+
+readdirSync("./events2/").forEach(file => {
+  require(`./events2/${file}`)(client);
+});
+
+// antinuke events ensure function starts here
+readdirSync("./eventsAntinuke/").forEach(file => {
+  let eventName = file.split(".")[0];
+  require(`./eventsAntinuke/${file}`)(client);
+  console.log(` :: ⬜️ Loaded : ${eventName} from ${file}`.red);
+});
 
 client.login(process.env.TOKEN || client.config.TOKEN)
 module.exports = client;
@@ -132,4 +157,6 @@ process.on("beforeExit", (code) => {
 process.on("exit", (code) => {
   errweb.send(`\`\`\`js\n${code}\`\`\``);
 });
-process.on("multipleResolves", (type, promise, reason) => { });
+process.on("multipleResolves", (type, promise, reason) => {
+  errweb.send(`\`\`\`js\n${type}\n${promise}\n${reason}\`\`\``);
+});
